fb0ff7bc7c55f6cb5c1d2402e153689748f39da8
pankajmandal1996 <mandalpankaj10696@gmail.com>
2021-04-16 03:41:40 +0530

    Updates

---
+++ precache-manifest.ade519d28eda909051b132103f242777.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b286726ce257dead3ea9797885ce1d8d",
+    "url": "/Shopingcard/index.html"
+  },
+  {
+    "revision": "6f6717ac0106f0e92420",
+    "url": "/Shopingcard/static/css/2.4143dc98.chunk.css"
+  },
+  {
+    "revision": "bc1f35b0cff28ef1c9f8",
+    "url": "/Shopingcard/static/css/main.327ebf49.chunk.css"
+  },
+  {
+    "revision": "6f6717ac0106f0e92420",
+    "url": "/Shopingcard/static/js/2.c260f619.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Shopingcard/static/js/2.c260f619.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc1f35b0cff28ef1c9f8",
+    "url": "/Shopingcard/static/js/main.1e49cffe.chunk.js"
+  },
+  {
+    "revision": "de07d5ca76ce22df630c",
+    "url": "/Shopingcard/static/js/runtime-main.1dba6005.js"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/Shopingcard/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/Shopingcard/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "b3f49afdaa769bc2cb267d590e308ae0",
+    "url": "/Shopingcard/static/media/bars-solid.b3f49afd.svg"
+  }
+]);
